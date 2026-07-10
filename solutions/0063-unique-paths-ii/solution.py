556308f3755208da6c3db784d8622a2b17ca4533
OlmanHerrera <martinezolmanalejandro@gmail.com>
2026-07-10 02:43:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 23.4 MB (5.21%)

---
+++ solutions/0063-unique-paths-ii/solution.py
@@ -0,0 +1,20 @@
+import numpy as np
+class Solution:
+    def uniquePathsWithObstacles(self, grid):
+        m,n = len(grid), len(grid[0])
+        dp = [0]* n 
+        dp[n-1] = 1 
+        for r in reversed(range(m)):
+            for c in reversed(range(n)):
+                if grid[r][c]:
+                    dp[c] = 0
+                elif c< n-1:
+                    dp[c] = dp[c]+ dp[c+1]
+        return dp[0]
+
+
+
+
+
+
+
